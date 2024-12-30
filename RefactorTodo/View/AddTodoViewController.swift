155ac30ab8b496d8a3dc68c457096fc053c81f05
MadCow <lobasketve@naver.com>
2024-12-30 23:19:49 +0900

    Feat. 오늘의 일기 작성 TextView 추가

--- RefactorTodo/View/AddTodoViewController.swift
+++ RefactorTodo/View/AddTodoViewController.swift
@@ -10,6 +10,18 @@ class AddTodoViewController: UIViewController {
     private lazy var dateLabel = TodoLabel(text: Date().dateToString(date: selectedDate),
                                       textColor: .lightGray,
                                       fontWeight: .bold)
+    private let placeholderText = "오늘의 일기를 작성해주세요"
+    private lazy var todoContent: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 15)
+        textView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        textView.backgroundColor = .white
+        textView.text = "오늘의 일기를 작성해주세요"
+        textView.textColor = textView.text == "오늘의 일기를 작성해주세요" ? .lightGray : .black
+        textView.delegate = self
+        
+        return textView
+    }()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -27,6 +39,7 @@ class AddTodoViewController: UIViewController {
         navigationController?.tabBarController?.isTabBarHidden = true
         configureButton()
         configureLabel()
+        configureTextView()
     }
     
     func configureButton() {
@@ -44,6 +57,16 @@ class AddTodoViewController: UIViewController {
             $0.centerX.equalTo(view.snp.centerX)
         }
     }
+    
+    func configureTextView() {
+        view.addSubview(todoContent)
+        todoContent.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(10)
+            $0.left.equalTo(view.snp.left).offset(10)
+            $0.right.equalTo(view.snp.right).offset(-10)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
 }
 
 extension AddTodoViewController: View {
@@ -52,5 +75,22 @@ extension AddTodoViewController: View {
             .map { Reactor.Action.showEmotionView(self.selectedDate ?? Date()) }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
+    }
+}
+
+extension AddTodoViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == placeholderText {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+       
+   // 텍스트뷰 편집 종료할 때
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+           textView.text = placeholderText
+           textView.textColor = .lightGray
+        }
     }
 }
