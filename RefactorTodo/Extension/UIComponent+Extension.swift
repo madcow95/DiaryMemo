8f785da287e7fbe712332fa58db463485c398dd2
MadCow <lobasketve@naver.com>
2025-01-17 02:09:41 +0900

    Feat. 슬라이더 UI 재구성

--- RefactorTodo/Extension/UIComponent+Extension.swift
+++ RefactorTodo/Extension/UIComponent+Extension.swift
@@ -1,33 +1,114 @@
 import UIKit
+import SnapKit
 
-class FontSizeSlider: UISlider {
+class FontSizeSlider: UIView {
+    private lazy var minusButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "minus"), for: .normal)
+        button.tintColor = .primaryColor
+        
+        return button
+    }()
+    lazy var hLine: UIView = {
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = .lightGray
+        
+        return line
+    }()
+    private lazy var plusButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = .primaryColor
+        
+        return button
+    }()
+    private var count: Int = 0
+    private var index: Int = 3
+    private var prevCircle: UIButton?
+    
+    convenience init(
+        count: Int,
+        index: Int
+    ) {
+        self.init()
+        self.count = count
+        self.index = index
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupSlider()
+        initialSlider()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupSlider()
+        initialSlider()
     }
     
-    private func setupSlider() {
+    func initialSlider() {
         self.translatesAutoresizingMaskIntoConstraints = false
-        minimumValue = 0
-        maximumValue = 6
+        self.snp.makeConstraints {
+            $0.height.equalTo(44)
+        }
+        self.addSubview(minusButton)
+        self.addSubview(plusButton)
+        self.addSubview(hLine)
+        minusButton.snp.makeConstraints {
+            $0.left.equalTo(self.snp.left)
+            $0.centerY.equalTo(self.snp.centerY)
+            $0.width.equalTo(25)
+            $0.height.equalTo(25)
+        }
         
-        let trackImage = UIImage(systemName: "circle.fill")?.withTintColor(.systemGray, renderingMode: .alwaysTemplate)
-        setMinimumTrackImage(trackImage, for: .normal)
-        setMaximumTrackImage(trackImage, for: .normal)
+        hLine.snp.makeConstraints {
+            $0.left.equalTo(minusButton.snp.right).offset(10)
+            $0.right.equalTo(plusButton.snp.left).offset(-10)
+            $0.centerY.equalTo(self.snp.centerY)
+            $0.height.equalTo(3)
+        }
         
-        let thumbImage = UIImage(named: "circle.fill")?.withTintColor(.white, renderingMode: .alwaysTemplate)
-        setThumbImage(thumbImage, for: .normal)
-        
-        addTarget(self, action: #selector(snapToValue), for: .valueChanged)
+        plusButton.snp.makeConstraints {
+            $0.right.equalTo(self.snp.right)
+            $0.centerY.equalTo(self.snp.centerY)
+            $0.width.equalTo(25)
+            $0.height.equalTo(25)
+        }
     }
     
-    @objc private func snapToValue() {
-        let roundedValue = round(value)
-        setValue(roundedValue, animated: true)
+    func drawCircle(lineWidth: CGFloat) {
+        let xPoint: CGFloat = lineWidth / CGFloat(count - 1)
+        for i in 0..<self.count {
+            let circle = UIButton()
+            var height: CGFloat = 10
+            var tintColor: UIColor = .systemGray4
+            circle.translatesAutoresizingMaskIntoConstraints = false
+            circle.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+            
+            if i == index {
+                height = 20
+                tintColor = .primaryColor
+            }
+            
+            circle.tintColor = tintColor
+            self.addSubview(circle)
+            
+            circle.snp.makeConstraints {
+                if i == 0 {
+                    // 첫 번째 원의 centerX를 hLine의 left와 일치
+                    $0.centerX.equalTo(hLine.snp.left)
+                } else if i == self.count - 1 {
+                    // 마지막 원의 centerX를 hLine의 right와 일치
+                    $0.centerX.equalTo(hLine.snp.right)
+                } else {
+                    $0.left.equalTo(hLine.snp.left).offset(xPoint * CGFloat(i))
+                }
+                $0.centerY.equalTo(hLine)
+                $0.height.equalTo(height)
+                $0.width.equalTo(height)
+            }
+        }
     }
 }
--- RefactorTodo/View/SettingFontViewController.swift
+++ RefactorTodo/View/SettingFontViewController.swift
@@ -27,34 +27,7 @@ class SettingFontViewController: TodoViewController {
                                       textColor: .lightGray)
     private let firstPreviewLabel = TodoLabel(text: "일기를 쓰는 습관 :)")
     private let secondPreviewLabel = TodoLabel(text: "변경된 폰트 사이즈가 표시됩니다")
-    private lazy var slider: UISlider = {
-        let slider = UISlider()
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.minimumTrackTintColor = .systemGray4
-        slider.maximumTrackTintColor = .systemGray4
-        
-        let thumbSize: CGFloat = 15
-        let configuration = UIImage.SymbolConfiguration(pointSize: thumbSize)
-        let thumbImage = UIImage(systemName: "circle.fill")?
-            .withConfiguration(configuration)
-            .withTintColor(.clear, renderingMode: .alwaysOriginal)
-        slider.setThumbImage(thumbImage, for: .normal)
-        slider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
-        
-        slider.minimumValue = 0
-        slider.maximumValue = 6
-        slider.value = 3
-        
-        return slider
-    }()
-    private var tickButtons: [UIButton] = []
-    
-    private lazy var ticksContainerView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
+    private let slider = FontSizeSlider(count: 7, index: FontCase.normal.rawValue)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -107,89 +80,27 @@ class SettingFontViewController: TodoViewController {
     
     func configureSlider() {
         view.addSubview(slider)
-        view.addSubview(ticksContainerView)
-        
         slider.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(40)
-            $0.trailing.equalToSuperview().offset(-40)
-            $0.centerY.equalToSuperview()
+            $0.top.equalTo(previewContainer.snp.bottom).offset(40)
+            $0.left.equalTo(view.snp.left).offset(10)
+            $0.right.equalTo(view.snp.right).offset(-10)
         }
+
+        view.layoutIfNeeded()
         
-        ticksContainerView.snp.makeConstraints {
-            $0.leading.trailing.equalTo(slider)
-            $0.height.equalTo(10)
-            $0.centerY.equalTo(slider)
-        }
-        
-        setupTicks()
-    }
-    
-    func setupTicks() {
-        let tickCount = 7
-        
-        let totalWidth = view.frame.width - 80
-        let spacing = totalWidth / CGFloat(tickCount - 1)
-        
-        for i in 0..<tickCount {
-            var tickHeight: CGFloat = 10
-            let tickButton = UIButton()
-            tickButton.backgroundColor = .systemGray4
-            ticksContainerView.addSubview(tickButton)
-            
-            if i == reactor?.currentState.currentFontSize.rawValue {
-                tickHeight = 20
-                tickButton.backgroundColor = .primaryColor
-            }
-            tickButton.layer.cornerRadius = tickHeight / 2
-            
-            let xPosition = CGFloat(i) * spacing
-            tickButton.snp.makeConstraints {
-                $0.width.equalTo(tickHeight)
-                $0.height.equalTo(tickHeight)
-                $0.centerY.equalToSuperview()
-                $0.leading.equalToSuperview().offset(xPosition)
-            }
-            tickButton.rx.tap
-                .map { Reactor.Action.changeFontSize(FontCase.allCases[i]) }
-                .bind(to: reactor!.action)
-                .disposed(by: disposeBag)
-            
-            tickButtons.append(tickButton)
-        }
-    }
-    
-    @objc private func sliderValueChanged(_ sender: UISlider) {
-        let roundedValue = round(sender.value)
-        sender.value = roundedValue
+        slider.drawCircle(lineWidth: slider.hLine.frame.width)
     }
 }
 
 extension SettingFontViewController: View {
     func bind(reactor: SettingFontReactor) {
-        reactor.state.map { $0.currentFontSize.rawValue }
-            .observe(on: MainScheduler.instance)
-            .distinctUntilChanged()
-            .subscribe { [weak self] idx in
-                guard let self = self else { return }
-                if self.tickButtons.count > 0 {
-                    for i in 0..<7 {
-                        var tickHeight: CGFloat = 10
-                        let tickButton = self.tickButtons[i]
-                        tickButton.backgroundColor = .systemGray4
-                        
-                        if i == idx.element! {
-                            tickHeight = 20
-                            tickButton.backgroundColor = .primaryColor
-                        }
-                        tickButton.layer.cornerRadius = tickHeight / 2
-                        
-                        tickButton.snp.updateConstraints {
-                            $0.width.equalTo(tickHeight)
-                            $0.height.equalTo(tickHeight)
-                        }
-                    }
-                }
-            }
-            .disposed(by: disposeBag)
+//        reactor.state.map { $0.currentFontSize.rawValue }
+//            .observe(on: MainScheduler.instance)
+//            .distinctUntilChanged()
+//            .subscribe { [weak self] idx in
+//                guard let self = self else { return }
+//                
+//            }
+//            .disposed(by: disposeBag)
     }
 }
